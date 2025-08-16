faecb9ae6bfe60d04948a7f041c191a275b15f55
saratkumar43 <58083705+saratkumar43@users.noreply.github.com>
2025-08-16 10:51:08 -0400

    Update server.py

--- server.py
+++ server.py
@@ -27,7 +27,11 @@ def emot_detector():
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
 
-    return f"For the given statement, the system response is 'anger': {response['anger']}, "
+    return f"For the given statement, the system response is 'anger': \
+                    {response['anger']}, 'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, 'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. The dominant emotion is \
+                    {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
